4e518ead1aaf9266f13833dba967b57a4df6c7cf
Govindakafley11gmail <govindakafley11@gmail.com>
2025-12-18 22:45:37 +0600

    doing sequelize problem

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,23 +1,21 @@
+// 
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-dotenv.config(); // Load .env variables
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASS as string,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT as any, // e.g., "postgres"
-    logging: false,
-    pool: {
-      max: Number(process.env.DB_POOL_MAX),
-      min: Number(process.env.DB_POOL_MIN),
-      acquire: Number(process.env.DB_POOL_ACQUIRE),
-      idle: Number(process.env.DB_POOL_IDLE),
+const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
 
 export default sequelize;
