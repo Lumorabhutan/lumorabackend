42144fbddcec21916674057f2d2cf02585f388b4
Govindakafley11gmail <govindakafley11@gmail.com>
2025-12-18 22:48:47 +0600

    fix sequelize dialect and clean dist build

--- dist/config/db.js
+++ dist/config/db.js
@@ -1,20 +1,21 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
+// 
 const sequelize_1 = require("sequelize");
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config(); // Load .env variables
-const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT, // e.g., "postgres"
+const sequelize = new sequelize_1.Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
     logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
     pool: {
-        max: Number(process.env.DB_POOL_MAX),
-        min: Number(process.env.DB_POOL_MIN),
-        acquire: Number(process.env.DB_POOL_ACQUIRE),
-        idle: Number(process.env.DB_POOL_IDLE),
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
     },
 });
 exports.default = sequelize;
